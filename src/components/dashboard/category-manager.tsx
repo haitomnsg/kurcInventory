2580f114ab3b31a3674e544e35a3ac6f08dd02fe
Haitomns G <haitomns4173@gmail.com>
2025-08-10 02:40:39 +0000

    The add new category table is kinda looking wired its boders Her elongat

--- src/components/dashboard/category-manager.tsx
+++ src/components/dashboard/category-manager.tsx
@@ -53,7 +53,7 @@ export default function CategoryManager({ categories, onAdd, onUpdate, onDelete,
 
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
         <Card className="md:col-span-2">
             <CardHeader>
                 <div className="flex items-start sm:items-center justify-between gap-4 flex-col sm:flex-row">
